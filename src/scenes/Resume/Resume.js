dbd4fee0e679a3d6049d305e6c228f1735b748cc
Ryan McAndrews <rmcandre915@gmail.com>
2018-03-02 00:47:18 -0600

    Changed icon fade workings

--- src/scenes/Resume/Resume.js
+++ src/scenes/Resume/Resume.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import ResumeCard from "./components/ResumeCard/ResumeCard";
-import capitalOneLogo from '../../images/CapitalOneLogo.svg';
-import cloverAppsLogo from '../../images/CloverAppsLogo.svg';
 
 const Resume = () => {
 
     return (
         <div className="Resume pa3 pt4-ns">
             <div className="f4 f3-ns fw3">EXPERIENCE</div>
-            <ResumeCard 
-                svg={capitalOneLogo}
-                iconFill="red"
+            <ResumeCard
+                colorImage='images/CapitalOne_Color.svg'
+                grayImage='images/CapitalOne_Gray.svg'
                 company="Capital One"
                 title="Software Engineer">
                 Develop technology for Capital One’s card business as part of an agile team. Worked on Javascript front end, Java APIs, and cloud infrastructure.
             </ResumeCard>
             <ResumeCard 
-                svg={cloverAppsLogo}
-                iconFill="green"
+                colorImage='images/CloverApps_Color.svg'
+                grayImage='images/CloverApps_Gray.svg'
                 company="Clover Apps"
                 title="Intern">
                 Built iOS application from the bottom up for a client of the company.
--- src/scenes/Resume/components/ResumeCard/ResumeCard.js
+++ src/scenes/Resume/components/ResumeCard/ResumeCard.js
@@ -1,35 +1,44 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, {Component} from 'react';
+import FadeImage from '../../../../components/FadeImage/FadeImage';
 
-import SVGInline from "react-svg-inline"
+class ResumeCard extends Component {
 
-const ResumeCard = (props) => {
+    constructor(props) {
+        super(props);
+        let defaultHover = (window.innerWidth < 992)
+        this.state = {
+            hover: defaultHover
+        };
+    }
 
-    return (
-        <div className="ResumeCard tl mt3 card bg-white cf">
-            <div className="fl w-20">
-                <SVGInline 
-                    svg={props.svg}
-                    cleanup={true}
-                    className={`ResumeCard-icon ${props.iconFill}-fill`}/>
-            </div>
-            <div className="ResumeCard-text fl w-80 pr2 pb2">
-                <div>
-                    <span className="f4 f3-ns fw6">{props.company}</span>
-                    <span className="i f5 f4-ns"> -- {props.title}</span>
+    toggleHover() {
+        if(window.innerWidth < 992) {
+            return;
+        }
+        this.setState({ hover: !this.state.hover })
+    }
+
+    render() {
+        return (
+            <div className="ResumeCard tl mt3 card bg-white cf"
+                    onMouseEnter={() => this.toggleHover()} 
+                    onMouseLeave={() => this.toggleHover()}>
+                <div className="fl w-20 pl2 pl3-m pl4-l pt2 pb2">
+                    <FadeImage 
+                        src={this.state.hover ? this.props.colorImage : this.props.grayImage}
+                        style ={{ maxHeight: "8rem" }}/>
                 </div>
-                <div className="ResumeCard-text-details f5 f4-ns mt1 mt2-ns">{props.children}</div>
+                <div className="ResumeCard-text fl w-80 pr2 pb2">
+                    <div>
+                        <span className="f4 f3-ns fw6">{this.props.company}</span>
+                        <span className="i f5 f4-ns"> -- {this.props.title}</span>
+                    </div>
+                    <div className="ResumeCard-text-details f5 f4-ns mt1 mt2-ns">{this.props.children}</div>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 
 }
-
-ResumeCard.prototype = {
-    svg: PropTypes.string.isRequired,
-    company: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    children: PropTypes.string.isRequired
-};
 
 export default ResumeCard;
