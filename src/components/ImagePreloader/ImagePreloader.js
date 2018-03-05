46b10e1c02a06fdb6bfe1fd0d40b6adaa40ddad4
Ryan McAndrews <rmcandre915@gmail.com>
2018-03-04 22:33:37 -0600

    Making preloader work

--- src/components/ImagePreloader/ImagePreloader.js
+++ src/components/ImagePreloader/ImagePreloader.js
@@ -3,25 +3,25 @@ import React from 'react';
 const ImagePrelaoder = () => {
     return (
         <div className="hidden">
-            <img src="images/Angular_Color.svg" alt=""/>
-            <img src="images/AWS_Color.svg" alt=""/>
-            <img src="images/CapitalOne_Color.svg" alt=""/>
-            <img src="images/CloverApps_Color.svg" alt=""/>
-            <img src="images/Cucumber_Color.svg" alt=""/>
-            <img src="images/Docker_Color.svg" alt=""/>
-            <img src="images/Drools_Color.svg" alt=""/>
-            <img src="images/ECS_Color.svg" alt=""/>
-            <img src="images/Firebase_Color.svg" alt=""/>
-            <img src="images/Github_Color.svg" alt=""/>
-            <img src="images/Java_Color.svg" alt=""/>
-            <img src="images/Jenkins_Color.svg" alt=""/>
-            <img src="images/Lambda_Color.svg" alt=""/>
-            <img src="images/NodeJs_Color.svg" alt=""/>
-            <img src="images/React_Color.svg" alt=""/>
-            <img src="images/Reddit_Color.svg" alt=""/>
-            <img src="images/Ruby_Color.svg" alt=""/>
-            <img src="images/Spring_Color.svg" alt=""/>
-            <img src="images/SpringBoot_Color.svg" alt=""/>
+            <img src="images/skills/Angular_Color.svg" alt=""/>
+            <img src="images/skills/AWS_Color.svg" alt=""/>
+            <img src="images/skills/CapitalOne_Color.svg" alt=""/>
+            <img src="images/skills/CloverApps_Color.svg" alt=""/>
+            <img src="images/skills/Cucumber_Color.svg" alt=""/>
+            <img src="images/skills/Docker_Color.svg" alt=""/>
+            <img src="images/skills/Drools_Color.svg" alt=""/>
+            <img src="images/skills/ECS_Color.svg" alt=""/>
+            <img src="images/skills/Firebase_Color.svg" alt=""/>
+            <img src="images/skills/Github_Color.svg" alt=""/>
+            <img src="images/skills/Java_Color.svg" alt=""/>
+            <img src="images/skills/Jenkins_Color.svg" alt=""/>
+            <img src="images/skills/Lambda_Color.svg" alt=""/>
+            <img src="images/skills/NodeJS_Color.svg" alt=""/>
+            <img src="images/skills/React_Color.svg" alt=""/>
+            <img src="images/skills/Reddit_Color.svg" alt=""/>
+            <img src="images/skills/Ruby_Color.svg" alt=""/>
+            <img src="images/skills/Spring_Color.svg" alt=""/>
+            <img src="images/skills/SpringBoot_Color.svg" alt=""/>
         </div>
     )
 }
